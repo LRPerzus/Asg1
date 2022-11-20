4fc365f487ce3c7a4d8d54782c67be5d894e81c6
LRPerzus <s10242179@connect.np.edu.sg>
2022-11-20 14:07:44 +0800

    20/11 Save 1

---
+++ Potion.js
@@ -0,0 +1,13 @@
+class Potion {
+    constructor(
+        name,
+        hp,
+        cost
+    ){
+        this.name=name;
+        this.hp=hp;
+        this.cost=cost;
+    }
+}
+
+export default Potion
